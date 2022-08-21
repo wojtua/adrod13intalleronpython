374627f1cb2069412d29f9ea32980fb9f745a70f
Wojciech G <wojtulowedubbingi@gmail.com>
2022-08-21 23:17:05 +0200

    added main code
    added main code (its jsut for a meme on tiktok, not serious!)

---
+++ adrod13.py
@@ -0,0 +1,14 @@
+print("adrod13 intaller")
+print("")
+print("1. intall")
+print("2. dont intall")
+
+opcja = input("pick option ")
+
+if(opcja == "1"):
+    print("bozo yur pyton got bricked :skull:")
+
+if(opcja == "2"):
+    exit()
+
+
